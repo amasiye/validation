c59da81d51fba76c9a56f58347fcfca300bdea54
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-02-05 22:14:51 +0200

    update(rules): rename IsAlphaNumeric.php to IsAlphanumeric.php

--- src/Attributes/IsAlphanumeric.php
+++ src/Attributes/IsAlphanumeric.php
@@ -5,8 +5,11 @@ namespace Assegai\Validation\Attributes;
 use Assegai\Validation\Rules\AlphaNumericValidationRule;
 use Attribute;
 
+/**
+ * Checks if the string contains only letters and numbers.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
-class IsAlphaNumeric extends ValidationAttribute
+class IsAlphanumeric extends ValidationAttribute
 {
   public function __construct()
   {
