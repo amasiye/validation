397e89123065c35afa2e6b0d992dce7b57a05b68
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-02-06 22:29:52 +0200

    fix(attributes): add missing Attribute to IsNotEmpty.php

--- src/Attributes/IsNotEmpty.php
+++ src/Attributes/IsNotEmpty.php
@@ -3,7 +3,9 @@
 namespace Assegai\Validation\Attributes;
 
 use Assegai\Validation\Rules\NotEmptyValidationRule;
+use Attribute;
 
+#[Attribute(Attribute::TARGET_PROPERTY)]
 class IsNotEmpty extends ValidationAttribute
 {
   public function __construct()
